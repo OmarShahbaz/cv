42fad4e5548ff9f1cac2aa513a96f7ddb6e3d965
Omar Shahbaz <omarshahbaz7@gmail.com>
2023-09-09 23:19:00 +0500

    Added Header and education section

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html',
+    styleUrls:['./header.component.css'] 
+})
+export class HeaderComponent{
+    name : string = "Omar Shahbaz";
+    jobTitle : string = "Angular Developer";
+    email : string = "omarshahbaz@gmail.com";
+    phone : string = "+92 333 5716128";
+    socialLink : string = "https://www.linkedin.com/in/omar-shahbaz/";
+    location : string = "Islamabad, Pakistan";
+
+
+}
