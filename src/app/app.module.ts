7466468179b411c1dd8fd3e7235e9c218ca39784
Omar Shahbaz <omarshahbaz7@gmail.com>
2023-09-10 23:07:11 +0500

    structuring app design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CertificationsComponent } from './certifications/certifications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    EducationComponent
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    LanguagesComponent,
+    ProjectsComponent,
+    CertificationsComponent
   ],
   imports: [
     BrowserModule
