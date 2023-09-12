ecccc0f10dc383314f0f1a407a6614b30b1a910f
Omar Shahbaz <omarshahbaz7@gmail.com>
2023-09-12 23:45:45 +0500

    update experience section

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,9 +9,29 @@ import { Component } from '@angular/core';
 export class ExperienceComponent {
 
   public experience : string = "experience";
+  private _jobPosition : string="garbage";
+  private _company : string="Ericsson";
+  public _joiningDate = new Date(2022,11,7);
+  public location = 'Islamabad, Pakistan';
+
+  constructor(){}
 
   experienceTitle(){
     return this.experience.toUpperCase();
   }
 
+  get jobPosition() : string{
+    this._jobPosition="Jr. Angular Developer";
+    return this._jobPosition;
+  }
+
+  get companyName() : string{
+    this._company = "Ericsson Pakistan";
+    return this._company;
+  }
+
+  joiningDate(): string{
+    const formatedDate = `${(this._joiningDate.getMonth()).toString().padStart(2,'0')}-${this._joiningDate.getFullYear()}`;
+  return formatedDate;
+  }
 }
